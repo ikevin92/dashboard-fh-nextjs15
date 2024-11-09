ec8ba24faf6904a2095a080be3c08634e2e3d47d
KEVIN ECHEVERRI <ikevin1992@gmail.com>
2024-11-09 14:42:42 -0500

    generacion ssr

---
+++ src/app/dashboard/pokemons/page.tsx
@@ -0,0 +1,40 @@
+import { PokemonGrid, PokemonsResponse, SimplePokemon } from "@/pokemons";
+
+const getPokemons = async (
+  limit = 20,
+  offset = 0,
+): Promise<SimplePokemon[]> => {
+  const response = await fetch(
+    `https://pokeapi.co/api/v2/pokemon?${new URLSearchParams({
+      limit: limit.toString(),
+      offset: offset.toString(),
+    })}`,
+  );
+  const data: PokemonsResponse = await response.json();
+
+  const pokemons = data.results.map((pokemon) => {
+    return {
+      id: pokemon.url.split("/").at(-2)!,
+      name: pokemon.name,
+    };
+  });
+
+  // throw new Error("Error al cargar los pokemons");
+  // throw notFound();
+
+  return pokemons;
+};
+
+export default async function PokemonsPage() {
+  const pokemons = await getPokemons(151);
+
+  return (
+    <div className="flex flex-col">
+      <span className="text-5xl my-2">
+        Listado de Pokemons <small>estatico</small>
+      </span>
+
+      <PokemonGrid pokemons={pokemons} />
+    </div>
+  );
+}
--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -1,5 +1,10 @@
 import Image from "next/image";
-import { IoBrowsersOutline, IoCalculator, IoLogoReact } from "react-icons/io5";
+import {
+  IoBrowsersOutline,
+  IoCalculator,
+  IoFootball,
+  IoLogoReact,
+} from "react-icons/io5";
 import { SideBarMenuItem } from "./SideBarMenuItem";
 
 const menuItems = [
@@ -15,13 +20,19 @@ const menuItems = [
     title: "Counter",
     subTitle: "Contador client side",
   },
+  {
+    path: "/dashboard/pokemons",
+    icon: <IoFootball size={40} />,
+    title: "Pokemons",
+    subTitle: "Generación estática",
+  },
 ];
 export const Sidebar = () => {
   return (
     <div
       id="menu"
       style={{ width: "400px" }}
-      className="bg-gray-900 min-h-screen z-10 text-slate-300 w-64 left-0 h-screen overflow-y-scroll"
+      className="bg-gray-900 min-h-screen z-10 text-slate-300 w-64 left-0  overflow-y-scroll"
     >
       <div
         id="logo"
